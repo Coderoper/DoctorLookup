bb08dda163fe6c53e7ebca76a517516df8f3a02d
Guest User <Guest@epicodus-191.local>
2018-06-01 14:23:14 -0700

    this gets data from api and writes to the webpage

--- src/main.js
+++ src/main.js
@@ -6,25 +6,34 @@ import {DoctorLookup} from './doctor-lookup.js'
 
 $(document).ready(function() {
  $('#MedCondition').click(function() {
-   $('#condition').val("");
+
   //  let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=sore%20throat&location=47.602084%2C%20-122.333312%2C100&user_location=47.602084%2C%20-122.333312&skip=0&limit=10&user_key=286ec2bfb7ac3a5693ba2b0be2a099eb`;
    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}=47.602084%2C%20-122.333312%2C100&user_location=47.602084%2C%20-122.333312&skip=0&limit=10&user_key=286ec2bfb7ac3a5693ba2b0be2a099eb`;
    let request = new XMLHttpRequest();
-   let condition = $('#condition').val();
+   let condition = $("#condition").val();
+
+   console.log(">>>>>>>>>>>>>>>>condition" + condition)
    request.onreadystatechange = function() {
      if (this.readyState === 4 && this.status === 200) {
        console.log("DONE");
+       console.log(condition);
+
        let response = JSON.parse(this.responseText);
        getElements(response);
+
      }
    }
+
 
    request.open("GET", url, true);
    request.send();
 
    let getElements = function(response) {
      console.log(response);
-     $('.condition').text(`The condition is ${condition}  ${response.practices[0].location_slug}`);
+     console.log(`For the symyptom ${condition} these are the available doctors:`)
+     $('.condition').text(`For the symyptom ${condition} these are the available doctors:`)
+     $('.firstName').text(`First name: ${response.data[0].profile.first_name}`)
+    //  $('.condition').text(`The doctors name is ${response.data[0].profile.first_name}`);
 
    }
  });
